47bf078539caf122bc25f3641c8b6fd380f52f63
Gosia Kwapniewska <m.kwapniewska@beinformed.com>
2025-08-13 13:36:17 +0200

    Improve Response format

--- app/main.py
+++ app/main.py
@@ -4,14 +4,15 @@ import tempfile
 import os
 
 from app.services.llm_extraction import llm_extract
-from app.services.ocr_service import ocr_extract_text
+from app.services.ocr_service import ocr_and_structure
+from app.schema.Response import Response
 
 
 app = FastAPI(title="Invoice Extraction PoC")
 
 
 @app.post("/extract")
-async def extract_invoice(file: UploadFile = File(...), mode: str = Form("llm"), model: str = "google/gemini-2.5-flash"):
+async def extract_invoice(file: UploadFile = File(...), mode: str = Form("llm"), model_ocr: str = "google/gemini-2.5-flash", model_llm: str = "google/gemini-2.5-flash"):
     # Save temp file
     with tempfile.NamedTemporaryFile(delete=False, suffix=f"_{file.filename}") as tmp:
         tmp.write(await file.read())
@@ -19,15 +20,15 @@ async def extract_invoice(file: UploadFile = File(...), mode: str = Form("llm"),
 
     try:
         if mode == "ocr":
-            text = ocr_extract_text(tmp_path)
-            return JSONResponse(content={"raw_text": text})
+            result = ocr_and_structure(tmp_path, model_ocr)
+            return Response(structured_data= result, model=model_ocr, method='ocr')
         elif mode == "llm":
-            result = llm_extract(tmp_path, model)
-            return JSONResponse(content={"structured_data": result})
+            result = llm_extract(tmp_path, model_llm)
+            return Response(structured_data= result, model=model_llm, method='llm')
         elif mode == "both":
-            text = ocr_extract_text(tmp_path)
-            result = llm_extract(tmp_path, model)
-            return JSONResponse(content={"ocr_text": text, "structured_data": result})
+            text = ocr_and_structure(tmp_path, model_ocr)
+            result = llm_extract(tmp_path, model_llm)
+            return {'OCR': Response(structured_data=text, model=model_ocr, method='ocr'), 'LLM': Response(structured_data=result, model=model_llm, method='llm')}
         else:
             return JSONResponse(content={"error": "Invalid mode"}, status_code=400)
     finally:
---
+++ app/schema/Response.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+from typing import Dict
+
+class Response(BaseModel):
+    structured_data: Dict
+    model: str = None
+    method: str
